13913b4459d7e0f135fef3f831e404acf8753d24
Ben Rogers <btorogers@live.co.uk>
2018-05-26 12:38:57 +0100

    removed crashing analytics fragment for testing

--- app/src/main/java/com/group14/events_near_me/FirebaseController.java
+++ app/src/main/java/com/group14/events_near_me/FirebaseController.java
@@ -28,7 +28,7 @@ public class FirebaseController {
     public FirebaseController() {
         firebaseAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance("https://com2027-group14-1519651413217.firebaseio.com/");
-        database.setPersistenceEnabled(true);
+        database.setPersistenceEnabled(false);
         ref = database.getReference();
     }
 
